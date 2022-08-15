f058bd09572212ad611ed4f09b26a635da209395
Vicwell28 <vicwell.bj@gmail.com>
2022-08-14 20:03:56 -0500

    init

---
+++ ProyectoTwitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoTwitter
+//
+//  Created by soliduSystem on 14/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
